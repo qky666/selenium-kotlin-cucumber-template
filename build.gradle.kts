e5f4d80ee74205b5adec9c0191077fd701143b80
qky <qky669@gmail.com>
2023-05-24 12:28:56 +0200

    v0.18.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import ru.vyarus.gradle.plugin.python.task.PythonTask
 
 group = "es.mtp"
-version = "0.17.1"
+version = "0.18.3"
 
 repositories {
     mavenCentral()
@@ -12,20 +12,19 @@ plugins {
     kotlin("jvm") version "1.8.20"
     id("io.qameta.allure") version "2.11.2"
     id("com.github.ben-manes.versions") version "0.46.0"
-    id("org.jlleitschuh.gradle.ktlint") version "11.3.1"
+    id("org.jlleitschuh.gradle.ktlint") version "11.3.2"
     id("ru.vyarus.use-python") version "3.0.0"
 }
 
 dependencies {
     val log4jVersion = "2.20.0"
-    val cucumberVersion = "7.11.2"
+    val cucumberVersion = "7.12.0"
 
-    testImplementation("org.testng:testng:7.7.1")
-    testImplementation("com.codeborne:selenide:6.13.1")
-    testImplementation("com.github.qky666:selenide-pom:0.17.1")
+    testImplementation("org.testng:testng:7.8.0")
+    testImplementation("com.github.qky666:selenide-pom:0.18.3")
     testImplementation("io.cucumber:cucumber-java8:$cucumberVersion")
     testImplementation("io.cucumber:cucumber-testng:$cucumberVersion")
-    testImplementation("io.qameta.allure:allure-cucumber7-jvm:2.21.0")
+    testImplementation("io.qameta.allure:allure-cucumber7-jvm:2.22.1")
     testImplementation("org.apache.logging.log4j:log4j-core:$log4jVersion")
     testImplementation("org.apache.logging.log4j:log4j-slf4j2-impl:$log4jVersion")
     testImplementation("org.apache.logging.log4j:log4j-api-kotlin:1.2.0")
@@ -49,7 +48,11 @@ kotlin {
     jvmToolchain(17)
 }
 
-tasks.withType<JavaCompile> {
+tasks.compileTestKotlin {
+    kotlinOptions.freeCompilerArgs += "-Xjvm-default=all"
+}
+
+tasks.compileTestJava {
     options.compilerArgs.addAll(listOf("-encoding", "UTF-8"))
 }
 
